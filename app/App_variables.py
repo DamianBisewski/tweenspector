6bd2a2b7e739f34c1202f9d8e3bad63369513912
Mbuzak <58588944+Mbuzak@users.noreply.github.com>
2021-12-28 15:32:00 +0100

    GUI part

---
+++ app/App_variables.py
@@ -0,0 +1,17 @@
+# fonts
+title_font = ("Segoe UI Semibold", 20)
+large_font = ('Segoe UI Semibold', 13)
+small_font = ('Segoe UI', 11)
+
+# colors
+bg = '#313131'  # background color
+
+# features
+features = {"":
+                "UWAGA: wybierz funkcjonalność",
+            "Najczęstsze słowa":
+                "Opis funkcjonalności: zbiór najczęściej używanych słów dla danego użytkownika Twittera",
+            "Powiązane konta":
+                "Opis funkcjonalności: sieć powiązanych kont z danym użytkownikiem Twittera",
+            "Konta podobne":
+                "Opis funkcjonalności: lista kont podobnych z podanym użytkownikiem Twittera"}
---
+++ app/TweetsData.py
@@ -0,0 +1,48 @@
+import twint
+from wordcloud import WordCloud, STOPWORDS
+import morfeusz2
+import matplotlib.pyplot as plt
+
+
+class TweetsData:
+    def __init__(self, username, num_of_tweets=500):
+        self.morf = morfeusz2.Morfeusz()
+        self.username = username
+        self.num_of_tweets = num_of_tweets
+
+    def get_tweets(self):
+        try:
+            c = twint.Config()
+            c.Username = self.username
+            c.Limit = self.num_of_tweets
+            c.Pandas = True
+            c.Pandas_clean = True
+            print(twint.run.Search(c))
+            return twint.output.panda.Tweets_df[["username", "tweet"]]
+        except ValueError:
+            print("Taki uzytkownik nie istnieje")
+
+    def generate_word_cloud(self):
+        tweets = self.get_tweets()
+        try:
+            text1 = tweets.tweet.values
+            stopwords = set(STOPWORDS)
+            file = open("stopwords.txt", "r", encoding="utf8")
+            word = file.readline().replace('\n', '')
+            while word:
+                stopwords.add(word)
+                word = file.readline().replace('\n', '')
+            wordcloud = WordCloud(
+                background_color='black',
+                width=1000,
+                height=500,
+                stopwords=stopwords).generate(str(text1))
+            wordcloud.to_file("file.png")
+        except ValueError:
+            print("warning")
+
+    # users account connections feature
+    def generate_another_one(self):
+        plt.figure()
+        plt.suptitle("Hello another one!")
+        plt.savefig("file.png")
