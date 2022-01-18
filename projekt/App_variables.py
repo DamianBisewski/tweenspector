532435e154bf7365707411967f0627dc31278861
Mbuzak <58588944+Mbuzak@users.noreply.github.com>
2022-01-18 12:05:30 +0100

    Delete projekt directory

--- projekt/App_variables.py
+++
@@ -1,9 +0,0 @@
-# fonts
-large_font = ('Segoe UI Semibold', 13)
-small_font = ('Segoe UI', 10)
-
-# colors
-bg = '#313131'  # background color
-
-# features name
-features = ['user word connections', 'related people connection', 'user similar accounts']
